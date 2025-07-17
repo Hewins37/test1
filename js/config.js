7f24d4d272cae081b7508b7c67951d0c2e8eeb0a
Hewins37 <m.moise1937@gmail.com>
2025-07-17 14:06:51 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_yyjUGe1M7', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '7v4kp182qsj6flhaf4f47nf38d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-eat-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
